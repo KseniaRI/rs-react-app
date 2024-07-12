767335d36a0baaf3af545148f8535655ca119f76
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-07-12 19:46:50 +0200

    feat: add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,20 @@
 import globals from "globals";
-import reactCompilerPlugin  from 'eslint-plugin-react-compiler';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from'@typescript-eslint/parser';
+import reactCompilerPlugin from "eslint-plugin-react-compiler";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import hooksPlugin from "eslint-plugin-react-hooks";
+import prettierPlugin from "eslint-plugin-prettier";
 import js from "@eslint/js";
 
 export default [
   {
-    ignores:[ 'eslint.config.cjs', 'dist' ],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ["eslint.config.cjs", "dist"],
+    files: ["**/*.{ts,tsx}"],
     plugins: {
-      'react-compiler': reactCompilerPlugin ,
+      "react-compiler": reactCompilerPlugin,
       "react-hooks": hooksPlugin,
-      "@typescript-eslint": tseslint
+      "@typescript-eslint": tseslint,
+      prettierPlugin,
     },
     languageOptions: {
       parser: tsParser,
@@ -30,8 +32,8 @@ export default [
       ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
       ...hooksPlugin.configs.recommended.rules,
-      '@typescript-eslint/no-explicit-any': ['error'],
-      'react-compiler/react-compiler': ['error'],
-     },
-  }
-]
+      "@typescript-eslint/no-explicit-any": ["error"],
+      "react-compiler/react-compiler": ["error"],
+    },
+  },
+];
