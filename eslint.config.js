7e2fb1f50cd95835e2509044bc616aa17d9c9c6c
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-07-12 19:34:10 +0200

    feat: add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import globals from "globals";
+import reactCompilerPlugin  from 'eslint-plugin-react-compiler';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from'@typescript-eslint/parser';
+import hooksPlugin from "eslint-plugin-react-hooks";
+import js from "@eslint/js";
+
+export default [
+  {
+    ignores:[ 'eslint.config.cjs', 'dist' ],
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-compiler': reactCompilerPlugin ,
+      "react-hooks": hooksPlugin,
+      "@typescript-eslint": tseslint
+    },
+    languageOptions: {
+      parser: tsParser,
+      // parserOptions: {
+      //   ecmaFeatures: {
+      //     jsx: true,
+      //   },
+      // },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...hooksPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': ['error'],
+      'react-compiler/react-compiler': ['error'],
+     },
+  }
+]
